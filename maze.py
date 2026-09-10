945635e88a5e057e5ef5ac000544f51fb4ed2cb5
juniorkaieti <junior.kaieti@sbhsstudent.school.nz>
2026-09-11 10:46:28 +1200

    Added text saying Waharoa Maze

--- maze.py
+++ maze.py
@@ -1,19 +1,25 @@
 import pygame
+
+#window screen
 WIDTH, HEIGHT, FPS = 800, 600, 60
 NAVY = (9, 24, 43)
+WHITE = (255, 255, 255)
 def run_game():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Star Catcher")
+    pygame.display.set_caption("Waharoa Maze")
     clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 70)
     running = True
     while running:
-      clock.tick(FPS)
-      for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-             running = False
-    screen.fill(NAVY)
-    pygame.display.flip()
-pygame.quit()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(NAVY)
+        title = font.render("Waharoa Maze", True, WHITE)
+        screen.blit(title, (250, 250))
+        pygame.display.flip()
+    pygame.quit()
 if __name__ == "__main__":
-   run_game()
+    run_game()
